65070464c4fccc0b88ed93d027590d9d78a15c66
xaes117 <xaes@protonmail.com>
2021-01-30 06:28:01 +0000

    further test templates

---
+++ Platform.Tests/Models/Assets/ReviewTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Platform.Tests.Models.Assets
+{
+    [TestClass]
+    public class ReviewTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
