37c4fe323f0043c8b9728372c37bb25241a22867
xaes117 <xaes@protonmail.com>
2021-02-01 17:02:35 +0000

    register user mock tests

--- Platform.Tests/Controllers/LoginControllerTest.cs
+++ Platform.Tests/Controllers/LoginControllerTest.cs
@@ -8,6 +8,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Platform;
 using Platform.Controllers;
 using Moq;
+using Platform.Models;
 
 namespace Platform.Tests.Controllers
 {
@@ -67,6 +68,44 @@ namespace Platform.Tests.Controllers
 
             // Assert
             Assert.AreNotEqual("OK", negativeTest);
+        }
+
+        [TestMethod]
+        public void RegisterPasswordTest()
+        {
+            // Arrange
+            LoginController controller = new LoginController();
+
+            // Act
+            string out1 = controller.Post("{'type': 'register', 'user': 'username', password: 'password'}");
+            string out2 = controller.Post("{'type': 'register', 'user': 'username', password: 'password123'}");
+            string out3 = controller.Post("{'type': 'register', 'user': 'username', password: 'password@'}");
+            string out4 = controller.Post("{'type': 'register', 'user': 'username', password: 'passwo'}");
+
+            // Assert
+            Assert.AreNotEqual("OK", out1);
+            Assert.AreNotEqual("OK", out2);
+            Assert.AreNotEqual("OK", out3);
+            Assert.AreNotEqual("OK", out4);
+        }
+
+        [TestMethod]
+        public void RegisterUserTest()
+        {
+            // Arrange
+            Mock<DataManager> dataManager = new Mock<DataManager>();
+            dataManager.Setup(x => x.userExists("user1")).Returns(true);
+            dataManager.Setup(x => x.userExists("user2")).Returns(false);
+
+            LoginController controller = new LoginController(dataManager.Object);
+
+            // Act
+            string user1 = controller.Post("{'type': 'register', 'user1': 'username', password: 'password123@'}");
+            string user2 = controller.Post("{'type': 'register', 'user2': 'username', password: 'password123@'}");
+
+            // Assert
+            Assert.AreEqual("OK", user1);
+            Assert.AreNotEqual("OK", user2);
         }
 
         [TestMethod]
