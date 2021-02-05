79c30ed4d789c7eb9be7ee906da07c5aecb2d713
xaes117 <xaes@protonmail.com>
2021-02-05 01:35:07 +0000

    DriverLib essential functions

---
+++ AcceptanceTesting/DriverLib/Driver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptanceTesting.DriverLib
+{
+    class Driver
+    {
+        // Click on a button
+        public static void Click(string buttonName)
+        {
+            
+        }
+
+        // Hit Enter key
+        public static void Enter()
+        {
+
+        }
+
+        // Enter text into a box
+        public static void TypeText(string text)
+        {
+
+        }
+    }
+}
