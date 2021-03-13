a7261ada9daa66154300d4af16091c89f36d61a0
xaes117 <xaes@protonmail.com>
2021-03-13 23:13:11 +0000

    userprofileController and test

---
+++ Platform.Tests/Controllers/UserProfilecontrollerTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Platform.Tests.Controllers
+{
+    [TestClass]
+    public class UserProfileTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
