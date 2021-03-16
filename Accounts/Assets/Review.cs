73aa068483f6ea99389941207eab9f3ad1bfb848
xaes117 <xaes@protonmail.com>
2021-03-16 22:35:05 +0000

    reviews

--- Accounts/Assets/Review.cs
+++ Accounts/Assets/Review.cs
@@ -7,5 +7,18 @@ namespace Accounts.Assets
 {
     public class Review
     {
+        // made public as private may affect object to json conversion
+        public string reviewer;
+        public string receiver;
+        public string rating;
+        public string description;
+
+        public Review(string reviewer, string receiver, string rating, string description)
+        {
+            this.reviewer = reviewer;
+            this.receiver = receiver;
+            this.rating = rating;
+            this.description = description;
+        }
     }
 }
